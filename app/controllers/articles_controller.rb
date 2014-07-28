6b35a928549000d550480e97cfa779b7d2675add
Learn Rails <learnrails@localhost.localdomain>
2014-07-28 17:05:51 -0400

    Add listing of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,12 @@ class ArticlesController < ApplicationController
 
   end
 
+  def index
+
+    @articles = Article.all
+    
+  end
+  
   private
   def article_params
     params.require(:article).permit(:title, :text)
