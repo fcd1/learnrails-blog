69dfb89fca17e8d16bc97af4cd5cf0520c056864
Learn Rails <learnrails@localhost.localdomain>
2014-07-28 16:52:38 -0400

    Add show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,12 @@ class ArticlesController < ApplicationController
 
   end
 
+  def show
+
+    @article = Article.find(params[:id])
+
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
